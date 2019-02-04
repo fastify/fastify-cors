3ea76ab85c0baa1314039bce0bd8cac5f71daa91
Jannik Keye <jannik.keye@gmail.com>
2019-02-04 14:41:48 +0100

    feat: add typings to repository

---
+++ test/types/index.ts
@@ -0,0 +1,90 @@
+import * as Fastify from 'fastify'
+import * as fastifyCors from '../..'
+
+const app = Fastify()
+
+app.register(fastifyCors, {
+  origin: true,
+  allowedHeaders: 'authorization,content-type',
+  methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+  credentials: true,
+  exposedHeaders: 'authorization',
+  maxAge: 13000,
+  preflightContinue: false,
+  optionsSuccessStatus: 200,
+  preflight: false
+})
+
+app.register(fastifyCors, {
+  origin: true,
+  allowedHeaders: ['authorization', 'content-type'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  credentials: true,
+  exposedHeaders: ['authorization'],
+  maxAge: 13000,
+  preflightContinue: false,
+  optionsSuccessStatus: 200,
+  preflight: false
+})
+
+app.register(fastifyCors, {
+  origin: '*',
+  allowedHeaders: ['authorization', 'content-type'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  credentials: true,
+  exposedHeaders: ['authorization'],
+  maxAge: 13000,
+  preflightContinue: false,
+  optionsSuccessStatus: 200,
+  preflight: false
+})
+
+app.register(fastifyCors, {
+  origin: /\*/,
+  allowedHeaders: ['authorization', 'content-type'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  credentials: true,
+  exposedHeaders: ['authorization'],
+  maxAge: 13000,
+  preflightContinue: false,
+  optionsSuccessStatus: 200,
+  preflight: false
+})
+
+app.register(fastifyCors, {
+  origin: ['*', 'something'],
+  allowedHeaders: ['authorization', 'content-type'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  credentials: true,
+  exposedHeaders: ['authorization'],
+  maxAge: 13000,
+  preflightContinue: false,
+  optionsSuccessStatus: 200,
+  preflight: false
+})
+
+app.register(fastifyCors, {
+  origin: [/\*/, /something/],
+  allowedHeaders: ['authorization', 'content-type'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  credentials: true,
+  exposedHeaders: ['authorization'],
+  maxAge: 13000,
+  preflightContinue: false,
+  optionsSuccessStatus: 200,
+  preflight: false
+})
+
+app.register(fastifyCors, {
+  origin: (err: Error, allow: boolean) => {
+    throw err
+  },
+  allowedHeaders: ['authorization', 'content-type'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  credentials: true,
+  exposedHeaders: ['authorization'],
+  maxAge: 13000,
+  preflightContinue: false,
+  optionsSuccessStatus: 200,
+  preflight: false
+})
