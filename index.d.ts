d8a222266151e16eade8a25fbeb7ad9fc688e3ac
Gabriel Cangussu <gabrielcangussu@gmail.com>
2020-03-23 20:26:24 -0300

    Add http2 server support on TS

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,7 @@
 // Definitions by: Jannik Keye <https://github.com/jannikkeye>
 
 import { Server, IncomingMessage, ServerResponse } from 'http'
+import { Http2Server, Http2ServerRequest, Http2ServerResponse } from 'http2'
 
 import fastify = require('fastify');
 
@@ -14,58 +15,63 @@ type ValueOrArray<T> = T | ArrayOfValueOrArray<T>;
 
 interface ArrayOfValueOrArray<T> extends Array<ValueOrArray<T>> {}
 
-declare const fastifyCors: fastify.Plugin<Server, IncomingMessage, ServerResponse, {
-    /**
-     * Configures the Access-Control-Allow-Origin CORS header.
-     */
-    origin?: ValueOrArray<originType> | originFunction;
-    /**
-     * Configures the Access-Control-Allow-Credentials CORS header.
-     * Set to true to pass the header, otherwise it is omitted.
-     */
-    credentials?: boolean;
-    /**
-     * Configures the Access-Control-Expose-Headers CORS header.
-     * Expects a comma-delimited string (ex: 'Content-Range,X-Content-Range')
-     * or an array (ex: ['Content-Range', 'X-Content-Range']).
-     * If not specified, no custom headers are exposed.
-     */
-    exposedHeaders?: string | string[];
-    /**
-     * Configures the Access-Control-Allow-Headers CORS header.
-     * Expects a comma-delimited string (ex: 'Content-Type,Authorization')
-     * or an array (ex: ['Content-Type', 'Authorization']). If not
-     * specified, defaults to reflecting the headers specified in the
-     * request's Access-Control-Request-Headers header.
-     */
-    allowedHeaders?: string | string[];
-    /**
-     * Configures the Access-Control-Allow-Methods CORS header.
-     * Expects a comma-delimited string (ex: 'GET,PUT,POST') or an array (ex: ['GET', 'PUT', 'POST']).
-     */
-    methods?: string | string[];
-    /**
-     * Configures the Access-Control-Max-Age CORS header.
-     * Set to an integer to pass the header, otherwise it is omitted.
-     */
-    maxAge?: number;
-    /**
-     * Pass the CORS preflight response to the route handler (default: false).
-     */
-    preflightContinue?: boolean;
-    /**
-     * Provides a status code to use for successful OPTIONS requests,
-     * since some legacy browsers (IE11, various SmartTVs) choke on 204.
-     */
-    optionsSuccessStatus?: number;
-    /**
-     * Pass the CORS preflight response to the route handler (default: false).
-     */
-    preflight?: boolean;
-    /**
-     * Hide options route from the documentation built using fastify-swagger (default: true).
-     */
-    hideOptionsRoute?: boolean;
-}>;
+declare const fastifyCors: fastify.Plugin<
+    Server | Http2Server,
+    IncomingMessage | Http2ServerRequest,
+    ServerResponse | Http2ServerResponse,
+    {
+        /**
+         * Configures the Access-Control-Allow-Origin CORS header.
+         */
+        origin?: ValueOrArray<originType> | originFunction;
+        /**
+         * Configures the Access-Control-Allow-Credentials CORS header.
+         * Set to true to pass the header, otherwise it is omitted.
+         */
+        credentials?: boolean;
+        /**
+         * Configures the Access-Control-Expose-Headers CORS header.
+         * Expects a comma-delimited string (ex: 'Content-Range,X-Content-Range')
+         * or an array (ex: ['Content-Range', 'X-Content-Range']).
+         * If not specified, no custom headers are exposed.
+         */
+        exposedHeaders?: string | string[];
+        /**
+         * Configures the Access-Control-Allow-Headers CORS header.
+         * Expects a comma-delimited string (ex: 'Content-Type,Authorization')
+         * or an array (ex: ['Content-Type', 'Authorization']). If not
+         * specified, defaults to reflecting the headers specified in the
+         * request's Access-Control-Request-Headers header.
+         */
+        allowedHeaders?: string | string[];
+        /**
+         * Configures the Access-Control-Allow-Methods CORS header.
+         * Expects a comma-delimited string (ex: 'GET,PUT,POST') or an array (ex: ['GET', 'PUT', 'POST']).
+         */
+        methods?: string | string[];
+        /**
+         * Configures the Access-Control-Max-Age CORS header.
+         * Set to an integer to pass the header, otherwise it is omitted.
+         */
+        maxAge?: number;
+        /**
+         * Pass the CORS preflight response to the route handler (default: false).
+         */
+        preflightContinue?: boolean;
+        /**
+         * Provides a status code to use for successful OPTIONS requests,
+         * since some legacy browsers (IE11, various SmartTVs) choke on 204.
+         */
+        optionsSuccessStatus?: number;
+        /**
+         * Pass the CORS preflight response to the route handler (default: false).
+         */
+        preflight?: boolean;
+        /**
+         * Hide options route from the documentation built using fastify-swagger (default: true).
+         */
+        hideOptionsRoute?: boolean;
+    }
+>;
 
 export = fastifyCors;
