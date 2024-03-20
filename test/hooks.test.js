6d678be45e622ec89fb1e531403b138fb83ab3f5
Mohammed Bilal Shareef <bilalshareef.h@gmail.com>
2024-03-20 13:33:47 +0530

    Update for v5

--- test/hooks.test.js
+++ test/hooks.test.js
@@ -9,7 +9,7 @@ test('Should error on invalid hook option', async (t) => {
   t.plan(1)
 
   const fastify = Fastify()
-  t.rejects(fastify.register(cors, { hook: 'invalid' }), new TypeError('@fastify/cors: Invalid hook option provided.'))
+  t.rejects(async () => fastify.register(cors, { hook: 'invalid' }), new TypeError('@fastify/cors: Invalid hook option provided.'))
 })
 
 test('Should set hook onRequest if hook option is not set', async (t) => {
